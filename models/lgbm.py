b7417a7f3fdd44800c42754f04cdea1ceedc8da3
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-11-28 19:00:09 -0500

    removed random spaces

--- models/lgbm.py
+++ models/lgbm.py
@@ -11,9 +11,9 @@ class LGBMModel:
 
     def train(self, features, target):
         target_encoded = self.label_encoder.fit_transform(target)
-        
+
         X_train, X_val, y_train, y_val = train_test_split(features, target_encoded, test_size=0.2, random_state=42)
-        
+
         self.model.fit(X_train, y_train)
 
         y_pred = self.model.predict(X_val)
